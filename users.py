bab035482ab0f8549bcf4e1f3c801504dc5b241c
6abef <139399618+6abef@users.noreply.github.com>
2023-11-18 01:13:30 -0600

    Language & decorators correction in users.py

--- users.py
+++ users.py
@@ -2,25 +2,29 @@
 This file contains the user class and the userslist
 """
 
-class User:
-    """User Class to register nickname, image and game id """
 
-    def __init__(self, nickname: str, image: int, gameid: int):
+class User:
+    """User Class to register nickname, image and game id"""
+
+    def __init__(self, userid, nickname: str, image: int, gameid: int):
+        self.userid = userid
         self.nickname = nickname
         self.image = image
-        self.gameid = int
+        self.gameid = gameid
 
     def __str__(self):
         """Print user's settings"""
         return f"({self.nickname}) {self.imagen} {self.gameid})"
-    
+
+
 class Users:
-    """List of defined users"""
-    lista:[User] = []
-    
-    @staticmethod
-    def crear(nickname: str, image: int, gameid) -> User:
+    """List of registered users"""
+
+    lista: list[User] = []
+
+    def new(self, nickname: str, image: int, gameid) -> User:
         """Method to register users objects type (nickname, image, game id) in users' list"""
-        user = User(nickname, image, gameid)
+        userid = len(self.lista) + 1
+        user = User(userid, nickname, image, gameid)
         Users.lista.append(user)
         return user
